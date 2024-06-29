ff9cf2128683e2ecb17973c5b39215e857deb335
travis <trng2200@gmail.com>
2024-06-29 11:16:46 -0500

    Initial commit

---
+++ src/db/btree.go
@@ -0,0 +1,14 @@
+package btree
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	if node1max > BTREE_PAGE_SIZE {
+		panic("node1max exceeds BTREE_PAGE_SIZE")
+	}
+}
---
+++ src/db/go.mod
@@ -0,0 +1,3 @@
+module github.com/tr-vs/db
+
+go 1.21.5
